069ee201b1faed2c677df7ec9948ff7b21dfaf7e
sharepointoscar <me@sharepointoscar.com>
2019-06-18 09:31:36 -0700

    fixed readfile, now using sync read instead

--- server.js
+++ server.js
@@ -4,24 +4,14 @@ var fileSystem = require('fs');
 var Rox = require('rox-node');
 var express = require('express');
 var app = express();
-
-
+var context= {};
 const appSettingsContainer = {
 	jenkinsx_environment: new Rox.Flag()
   };
 
-var context= { jenkinsx_environment: getJXEnvironment() };
-  console.log("-------------- getJXEnvironment() VALUE: "+getJXEnvironment()+'---------------------');
-
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
-
-// Routes - we pass two variables to the HTML to preform approrpiate actions based on conditions.
-app.get('/', function(req, res) {
-    res.render('pages/index',{env:context.jenkinsx_environment,renderButton:appSettingsContainer.jenkinsx_environment.isEnabled(context)});
-});
-
 
 Rox.setCustomStringProperty('JenkinsX Environment', function(context){
 	return context.jenkinsx_environment;
@@ -48,16 +38,13 @@ async function setupRox() {
  
  
  setupRox().then((value) => {
-	console.log('Rox returned value: '+ value);
-	console.log('Rox passed context:' + context.jenkinsx_environment);
-	console.log('setupBox() finished');
-	console.log(appSettingsContainer.jenkinsx_environment.isEnabled(context));
+	console.log('setupRox() returned value: '+ value);
 
 	if (appSettingsContainer.jenkinsx_environment.isEnabled(context)) {
-		console.log('We are in Staging Jenkins X environment!');
+		console.log('----- We are in Staging Jenkins X environment! --------');
 	 }
 	 else {
-		console.log('What Jenkins X environment? : '+ context.jenkinsx_environment);
+		console.log('------ What Jenkins X environment? : '+ context.jenkinsx_environment+' ---------');
 	 }
 	
  });
@@ -65,22 +52,29 @@ async function setupRox() {
 
 function getJXEnvironment() {
 	var _env = '';
-		
-	fileSystem.readFile('/var/run/secrets/kubernetes.io/serviceaccount/namespace', function (error, fileContent) {
-		if (error) {
-			console.log('getJXEnvironment(): hey there was an error reading the file: '+error);
-			_env = error;
-		}
+	_env = fileSystem.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/namespace', 'utf8');
+	// fileSystem.readFile('/var/run/secrets/kubernetes.io/serviceaccount/namespace', function (error, fileContent) {
+	// 	if (error) {
+	// 		console.log('getJXEnvironment(): hey there was an error reading the file: '+error);
+	// 		_env = error;
+	// 	}
 	
-		_env = fileContent;
-		console.log("getJXEnvrionment(): value:"+ _env);
+	// 	_env = fileContent;
+	// 	console.log("getJXEnvrionment(): value:"+ _env);
 		
-	});
-
+	// });
 	return _env;
 }
 
+// Routes - we pass two variables to the HTML to preform approrpiate actions based on conditions.
+app.get('/', function(req, res) {
+
+	// first ensure we have our file contents
+	context = { jenkinsx_environment: getJXEnvironment() };
+	console.log('----------- app.get() - called getJXEnvironment() and got: '+ context.jenkinsx_environment+' so rendering ---------------------');
+    res.render('pages/index',{env:context.jenkinsx_environment,renderButton:appSettingsContainer.jenkinsx_environment.isEnabled(context)});
+});
 
 app.listen(8080);
 
-console.log('Oh check this out, your app is listening on port 8080!');
+console.log('------ Ok your app is listening on port 8080! -------- ');
